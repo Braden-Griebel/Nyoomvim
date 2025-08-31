216fa77698df437185c7a467c50ebc45fe061f9d
Braden Griebel <bgriebel@uw.edu>
2025-08-31 13:17:28 -0700

    Initial flake setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Neovim Configuration Flake using NVF";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    nvf.url = "github:notashelf/nvf";
+  };
+
+  outputs = { nixpkgs, home-manager, nvf, ... }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    configModule = {
+      # Add any custom options (and do feel free to upstream them!)
+      # options = { ... };
+
+      config.vim = {
+        theme.enable = true;
+        # and more options as you see fit...
+      };
+    };
+
+    nyoomvim = nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      modules = [configModule];
+    };
+  in 
+  {
+    # This will make the package available as a flake output under 'packages'
+    packages.${system}.nyoomvim = nyoomvim.neovim;
+  };
+}

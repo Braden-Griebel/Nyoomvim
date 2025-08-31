0bf29a980a4e1b1d20d75da5462edda0cf7b029a
Braden Griebel <bgriebel@uw.edu>
2025-08-31 14:02:57 -0700

    Changing to standalong flake

--- flake.nix
+++ flake.nix
@@ -3,32 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    nvf.url = "github:notashelf/nvf";
+    nvf.url = {url="github:notashelf/nvf";inputs.nixpkgs.follows="nixpkgs";};
   };
+    outputs = {nixpkgs, ...} @ inputs: {
+    packages.x86_64-linux = {
+      # Set the default package to the wrapped instance of Neovim.
+      # This will allow running your Neovim configuration with
+      # `nix run` and in addition, sharing your configuration with
+      # other users in case your repository is public.
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            {
+              config.vim = {
+                # Enable custom theming options
+                theme.enable = true;
 
-  outputs = { nixpkgs, home-manager, nvf, ... }: 
-  let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    configModule = {
-      # Add any custom options (and do feel free to upstream them!)
-      # options = { ... };
+                # Enable Treesitter
+                treesitter.enable = true;
 
-      config.vim = {
-        theme.enable = true;
-        # and more options as you see fit...
-      };
+                # Other options will go here. Refer to the config
+                # reference in Appendix B of the nvf manual.
+                # ...
+              };
+            }
+          ];
+        })
+        .neovim;
     };
-
-    nyoomvim = nvf.lib.neovimConfiguration {
-      inherit pkgs;
-      modules = [configModule];
-    };
-  in 
-  {
-    # This will make the package available as a flake output under 'packages'
-    packages.${system}.default = nyoomvim.neovim;
-    nixosModules.default = nyoomvim.neovim;
   };
 }
